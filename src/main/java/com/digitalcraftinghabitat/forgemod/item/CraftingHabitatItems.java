71d9e868205b5e432f95e1c7a695b972c2b666ae
Christopher Ezell <raversound20@web.de>
2015-08-17 23:46:32 +0200

    improved Util Classes

---
+++ src/main/java/com/digitalcraftinghabitat/forgemod/item/CraftingHabitatItems.java
@@ -0,0 +1,9 @@
+package com.digitalcraftinghabitat.forgemod.item;
+
+/**
+ * Created by christopher on 16/08/15.
+ */
+public class CraftingHabitatItems {
+
+    public  static ItemCraftium itemCraftium = new ItemCraftium();
+}
