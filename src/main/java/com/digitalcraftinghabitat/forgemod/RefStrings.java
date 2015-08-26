463e6a3cd69d51443b5b7ad768c71c2a6270de3d
Christopher Ezell <raversound20@web.de>
2015-08-26 20:37:05 +0200

    aligned Mod Version in mcmod.info and code

--- src/main/java/com/digitalcraftinghabitat/forgemod/RefStrings.java
+++ src/main/java/com/digitalcraftinghabitat/forgemod/RefStrings.java
@@ -5,6 +5,5 @@ package com.digitalcraftinghabitat.forgemod;
  */
 public class RefStrings {
     public static final String MODID = "digitalcraftinghabitat";
-    public static final String VERSION = "1.0";
-
+    public static final String VERSION = "0.1";
 }
