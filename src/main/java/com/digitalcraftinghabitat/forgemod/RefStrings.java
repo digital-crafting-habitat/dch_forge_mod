de934607eefd304c67c89a9f9a2312a5d1ab45ee
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-09-01 22:43:58 +0200

    update Version

--- src/main/java/com/digitalcraftinghabitat/forgemod/RefStrings.java
+++ src/main/java/com/digitalcraftinghabitat/forgemod/RefStrings.java
@@ -5,5 +5,5 @@ package com.digitalcraftinghabitat.forgemod;
  */
 public class RefStrings {
     public static final String MODID = "digitalcraftinghabitat";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
 }
