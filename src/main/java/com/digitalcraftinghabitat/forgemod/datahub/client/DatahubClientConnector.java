1e9290b110a1e3d7bc38982fa020e0bacebfab54
marius.drodten <marius.drodten@valtech.de>
2015-09-14 14:40:16 +0200

    jedis auth

--- src/main/java/com/digitalcraftinghabitat/forgemod/datahub/client/DatahubClientConnector.java
+++ src/main/java/com/digitalcraftinghabitat/forgemod/datahub/client/DatahubClientConnector.java
@@ -49,6 +49,7 @@ public class DatahubClientConnector {
 
     public static void main(String args[]){
         Jedis jedis = new Jedis("85.214.235.74");
+        jedis.auth("DCH-Rocks-2015@");
         String redstone_value = jedis.get("redstone_value");
         System.out.println(redstone_value);
     }
