702105d6ee1bc6ecc23873d51379400f32bea0fc
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-09-16 00:18:09 +0200

    added Crafting Lever switch feature

--- src/main/java/com/digitalcraftinghabitat/forgemod/block/CraftingLever.java
+++ src/main/java/com/digitalcraftinghabitat/forgemod/block/CraftingLever.java
@@ -1,8 +1,10 @@
 package com.digitalcraftinghabitat.forgemod.block;
 
+import com.digitalcraftinghabitat.forgemod.datahub.client.DatahubClientConnector;
 import com.digitalcraftinghabitat.forgemod.util.DCHLog;
 import net.minecraft.block.BlockLever;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 /**
@@ -10,8 +12,13 @@ import net.minecraft.world.World;
  */
 public class CraftingLever extends BlockLever{
 
+    static DatahubClientConnector datahubClientConnector;
+
     public CraftingLever() {
         super();
+        if (datahubClientConnector == null){
+            datahubClientConnector = new DatahubClientConnector();
+        }
         setBlockName("crafting_lever_block");
         setCreativeTab(CreativeTabs.tabRedstone);
         this.setResistance(3.0F);
@@ -23,5 +30,20 @@ public class CraftingLever extends BlockLever{
         int superReturnValue =  super.onBlockPlaced(p_149660_1_, p_149660_2_, p_149660_3_, p_149660_4_, p_149660_5_, p_149660_6_, p_149660_7_, p_149660_8_, p_149660_9_);
         DCHLog.info("CraftingLever: it works");
         return superReturnValue;
+    }
+
+    @Override
+    public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
+        if (p_149727_1_.isRemote){
+            return true;
+        }
+        boolean bol = super.onBlockActivated(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, p_149727_5_, p_149727_6_, p_149727_7_, p_149727_8_, p_149727_9_);
+        int test = datahubClientConnector.getIntValueForKey("lever_value");
+        if (test == 1){
+            datahubClientConnector.setValueForKey("lever_value", "0");
+        }else {
+            datahubClientConnector.setValueForKey("lever_value", "1");
+        }
+        return bol;
     }
 }
