74c3448191abf269d0272340151cf01a99039bb5
Christopher Ezell <ezellchristopher85@googlemail.com>
2015-10-21 00:37:37 +0200

    new Password

--- src/main/java/com/digitalcraftinghabitat/forgemod/util/DCHConfiguration.java
+++ src/main/java/com/digitalcraftinghabitat/forgemod/util/DCHConfiguration.java
@@ -12,7 +12,7 @@ public class DCHConfiguration {
     public static final String JEDIS_URL = "jedis_url";
     public static final String JEDIS_AUTH = "jedis_auth";
     public static final String JEDIS_URL_DEFAULT = "85.214.235.74";
-    public static final String JEDIS_AUTH_DEFAULT = "DCH-Rocks-2015@";
+    public static final String JEDIS_AUTH_DEFAULT = "DCH-Rocks-2015@12321";
     private static final String JEDIS_PREFIX = "jedis_prefix";
     private static final String JEDIS_PREFIX_DEFAULT = "dev_";
     private String jedisAuth;
